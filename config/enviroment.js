13aa2ad5e159671d051ad51029d174de848c22a5
Enzo Vignolo <enzo.e.vignolo@gmail.com>
2021-09-01 12:56:19 -0300

    Adding env variables managing

--- config/enviroment.js
+++ config/enviroment.js
@@ -3,9 +3,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 let PORT, DB_URI;
-if (process.env.ENV == 'development') {
-	PORT = process.env.PORT_dev;
-	DB_URI = process.env.DB_URI_dev;
+if (process.env.ENV == 'development' || !process.env.ENV) {
+	PORT = process.env.PORT_dev || 5000;
+	DB_URI =
+		process.env.DB_URI_dev || 'mongodb://localhost:27017/ecommerce';
 } else if (process.env.ENV == 'production') {
 	PORT = process.env.PORT_prod;
 	DB_URI = process.env.DB_URI_prod;
